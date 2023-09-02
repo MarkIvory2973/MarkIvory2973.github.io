a4c045996b3fc4793a2cf70b15510a6db125cfc8
MarkIvory2973 <markivory2973@gmail.com>
2023-09-02 11:37:52 +0800

    Commit

--- api.py
+++ api.py
@@ -1,9 +1,18 @@
-import sys
+import os
 from flask import *
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/api/v1/blog", methods=["GET"])
 def root():
-    return "ROOT"
+    index = int(request.args.get("index", "0"))
+    blogs = list(os.scandir("blogs"))
+    
+    try:
+        with open(f"blogs/{blogs[index]}.md") as blog:
+            content = blog.read()
+    except FileNotFoundError:
+        abort(404)
+    
+    return len(blogs), content
 
 app.run("localhost", 5000)
