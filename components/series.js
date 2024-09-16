575a02fc604ea72835d1dca236b447e32ebbdfdc
Mark Ivory <markivory2973@gmail.com>
2024-09-16 22:27:58 +0800

    Commit

--- components/series.js
+++ components/series.js
@@ -1,19 +1,26 @@
 import Link from 'next/link'
 import styles from './series.module.css'
+import blog from '@/blog/blog'
 
 function Series() {
   return (
     <div id={styles.series} className={styles.v}>
         <h3>人工智能</h3>
         <div className={styles.links}>
-            <Link href='/blog/7' aria-label='神经网络如何自动调整'>神经网络如何自动调整</Link>
-            <Link href='/blog/5' aria-label='神经网络的本质'>神经网络的本质</Link>
+            <Link href='/blog/7' aria-label={blog[7].title}>{blog[7].title}</Link>
+            <Link href='/blog/5' aria-label={blog[5].title}>{blog[5].title}</Link>
+        </div>
+        <h3>数学</h3>
+        <div className={styles.links}>
+            <Link href='/blog/9' aria-label={blog[9].title}>{blog[9].title}</Link>
+            <Link href='/blog/4' aria-label={blog[4].title}>{blog[4].title}</Link>
+            <Link href='/blog/3' aria-label={blog[3].title}>{blog[3].title}</Link>
         </div>
         <h3>科学上网</h3>
         <div className={styles.links}>
-            <Link href='/blog/8' aria-label='如何安全地科学上网'>如何安全地科学上网</Link>
-            <Link href='/blog/6' aria-label='科学上网入门'>科学上网入门</Link>
-            <Link href='/blog/1' aria-label='为什么你需要抛弃 Clash for Windows'>为什么你需要抛弃 Clash for Windows</Link>
+            <Link href='/blog/8' aria-label={blog[8].title}>{blog[8].title}</Link>
+            <Link href='/blog/6' aria-label={blog[6].title}>{blog[6].title}</Link>
+            <Link href='/blog/1' aria-label={blog[1].title}>{blog[1].title}</Link>
         </div>
     </div>
   );
